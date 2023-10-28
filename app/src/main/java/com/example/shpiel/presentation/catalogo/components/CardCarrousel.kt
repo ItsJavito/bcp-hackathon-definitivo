3a2dae665742256db0f29d3f097123c5b54cd84f
Cristina <20192902@aloe.ulima.edu.pe>
2023-10-28 12:31:26 -0500

    scroll

--- app/src/main/java/com/example/shpiel/presentation/catalogo/components/CardCarrousel.kt
+++ app/src/main/java/com/example/shpiel/presentation/catalogo/components/CardCarrousel.kt
@@ -1,9 +1,11 @@
 package com.example.shpiel.presentation.catalogo.components
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,8 +16,9 @@ import com.example.shpiel.R
 @Composable
 fun CardCarrousel(){
     //val cardBackground = painterResource(id = R.drawable.background_image)
+    val scrollState = rememberScrollState()
 
-    Row {
+    Row(modifier = Modifier.horizontalScroll(scrollState)) {
         CardCarrouselItem("EcoTech ", "4.2", painterResource(id = R.drawable.verdeee))
         CardCarrouselItem("InfinityWave  ", "3.2", painterResource(id = R.drawable.teccc))
         CardCarrouselItem("SolarSolutions ", "4.8", painterResource(id = R.drawable.estrellas))
